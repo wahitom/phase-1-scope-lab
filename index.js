a30f95efb373bc8c99db78ca899d43ee662f2632
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-23 00:27:51 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    //return customerName
+}
+//console.log(upperCaseCustomerName())
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    //return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+    //return customerName;
+}
+
+const leastFavoriteCustomer = "actually bob"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "i may be wrong"
+    return leastFavoriteCustomer;
+}
